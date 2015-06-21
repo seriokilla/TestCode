00f4792f90f7eb0614affc044882ceaeba919aba
seriokilla <user@notebook1.com>
2015-06-21 17:20:47 -0400

    adding code for hotfix

--- ParseTest/Program.cs
+++ ParseTest/Program.cs
@@ -6,7 +6,35 @@ namespace ParseTest
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var d = Convert("1234567.6767890");
+            Console.WriteLine(d);
+        }
+
+        public static decimal Convert(string str)
+        {
+            decimal ret = 0M;
+            int order = 0;
+            bool isLeft = true;
+            foreach(var s in str)
+            {
+                if (s == '.')
+                {
+                    isLeft = false;
+                    order = 1;
+                    continue;
+                }
+                    
+                if (isLeft)
+                {
+                    ret = ret * (decimal)10;
+                    ret += ((int)s-(int)'0'); 
+                }
+                else
+                {
+                    ret += ((int)s-(int)'0')/((decimal)Math.Pow(10, order++));
+                }
+           }
+           return ret;
         }
     }
 }
