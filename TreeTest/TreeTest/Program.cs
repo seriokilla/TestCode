2ff00a24caf4cda61e6a0e40d48dd7ce8761f595
seriokilla <user@notebook1.com>
2015-06-21 16:20:51 -0400

    adding new project

---
+++ TreeTest/TreeTest/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TreeTest
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
